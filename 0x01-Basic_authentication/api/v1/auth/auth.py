521dd59d2d38e381e3fb32cb90e43d2427837f97
bayzman <bassmaninno102@gmail.com>
2024-11-13 17:12:17 +0300

    Auth class

--- 0x01-Basic_authentication/api/v1/auth/auth.py
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -7,16 +7,13 @@ from typing import List, TypeVar
 
 class Auth:
     """ Class Auth """
-
     def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
         """ Require auth """
         return False
 
-
     def authorization_header(self, request=None) -> str:
         """ Authorization header """
         return None
-
 
     def current_user(self, request=None) -> TypeVar('User'):
         """ Current user """
