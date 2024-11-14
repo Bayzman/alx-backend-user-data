daf887dcd23fe90920c22a53d6580b0e7360f359
bayzman <bassmaninno102@gmail.com>
2024-11-14 04:27:35 +0300

    Basic User credentials

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -39,3 +39,17 @@ class BasicAuth(Auth):
             return str(b64decode(base64_authorization_header), "utf-8")
         except Exception:
             return None
+
+    def extract_user_credentials(self, decoded_base64_authorization_header):
+        """ Extract user credentials """
+        if decoded_base64_authorization_header is None:
+            return None, None
+
+        if not isinstance(decoded_base64_authorization_header, str):
+            return None, None
+
+        if ":" not in decoded_base64_authorization_header:
+            return None, None
+
+        return decoded_base64_authorization_header.split(":")[0], \
+            decoded_base64_authorization_header.split(":")[1]
