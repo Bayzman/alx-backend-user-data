1b7ce786efa7d2a6f02ac7059e2b471ee89f390a
bayzman <bassmaninno102@gmail.com>
2024-11-20 13:13:28 +0300

    filtered logger

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -3,9 +3,11 @@
 """ Returns the log message obfuscated """
 
 import re
+from typing import List
 
 
-def filter_datum(fields, redaction, message, separator):
+def filter_datum(fields: List[str|int], redaction: List[str|int],
+                 message: List[str], separator: str) -> str:
     """ Returns the log message obfuscated """
     pattern = r'(' + '|'.join(fields) + r')=.*?' + re.escape(separator)
     return re.sub(pattern, lambda match:
