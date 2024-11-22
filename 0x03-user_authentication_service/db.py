401a79df718cd0d27f8088b9717cb1de1f7a4dd8
bayzman <bassmaninno102@gmail.com>
2024-11-22 04:31:18 +0300

    find user and update user

--- 0x03-user_authentication_service/db.py
+++ 0x03-user_authentication_service/db.py
@@ -6,6 +6,7 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.orm.session import Session
+from sqlalchemy.exc import InvalidRequestError, NoResultFound
 
 from user import User, Base
 
@@ -36,3 +37,19 @@ class DB:
         self._session.add(user)
         self._session.commit()
         return user
+
+    def find_user_by(self, **kwargs) -> User:
+        """ Find a user """
+        user = self._session.query(User).filter_by(**kwargs).first()
+        if user is None:
+            raise NoResultFound
+        return user
+
+    def update_user(self, user_id: int, **kwargs) -> None:
+        """ Update a user """
+        user: User = self.find_user_by(id=user_id)
+        for key, value in kwargs.items():
+            if hasattr(user, key) is False:
+                raise ValueError
+            setattr(user, key, value)
+        self._session.commit()
