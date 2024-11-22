971160d49a4445bd8d24543e92e284ba5ce1fb92
bayzman <bassmaninno102@gmail.com>
2024-11-22 04:53:07 +0300

    find user and update user

--- 0x03-user_authentication_service/db.py
+++ 0x03-user_authentication_service/db.py
@@ -1,23 +1,22 @@
 #!/usr/bin/env python3
-
-""" DB module """
-
+"""DB module
+"""
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.orm.session import Session
-from sqlalchemy.orm.exc import NoResultFound
-from sqlalchemy.exc import InvalidRequestError
 from user import User, Base
+from sqlalchemy.orm.exc import NoResultFound
 
 
 class DB:
-    """ DB class """
+    """DB class
+    """
 
     def __init__(self) -> None:
         """Initialize a new DB instance
         """
-        self._engine = create_engine("sqlite:///a.db", echo=True)
+        self._engine = create_engine("sqlite:///a.db", echo=False)
         Base.metadata.drop_all(self._engine)
         Base.metadata.create_all(self._engine)
         self.__session = None
@@ -32,21 +31,33 @@ class DB:
         return self.__session
 
     def add_user(self, email: str, hashed_password: str) -> User:
-        """ Add a new user """
+        """Add a user to the database.
+
+        Args:
+            email (str): The user's email address.
+            hashed_password (str): The user's hashed password.
+
+        Returns:
+            User: The user instance added to the database.
+        """
         user = User(email=email, hashed_password=hashed_password)
         self._session.add(user)
         self._session.commit()
         return user
 
     def find_user_by(self, **kwargs) -> User:
-        """ Find a user """
+        """
+        Find a user by any attribute
+        """
         user = self._session.query(User).filter_by(**kwargs).first()
         if user is None:
-            raise NoResultFound
+            raise NoResultFound()
         return user
 
-    def update_user(self, user_id: int, **kwargs) -> None:
-        """ Update a user """
+    def update_user(self, user_id: int, **kwargs):
+        """
+        Update a user
+        """
         user: User = self.find_user_by(id=user_id)
         for key, value in kwargs.items():
             if hasattr(user, key):
