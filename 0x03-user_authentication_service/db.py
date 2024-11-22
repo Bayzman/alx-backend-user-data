36123a0ff5a2107fdee81f8e88d6af505ed88bef
bayzman <bassmaninno102@gmail.com>
2024-11-22 05:03:19 +0300

    find user and update user

--- 0x03-user_authentication_service/db.py
+++ 0x03-user_authentication_service/db.py
@@ -42,7 +42,7 @@ class DB:
         """ Find a user """
         user = self._session.query(User).filter_by(**kwargs).first()
         if user is None:
-            raise NoResultFound()
+            raise NoResultFound
         return user
 
     def update_user(self, user_id: int, **kwargs) -> None:
