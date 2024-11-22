b796c36ed78ec120fdb9275791cc5487503f27b7
bayzman <bassmaninno102@gmail.com>
2024-11-22 06:32:17 +0300

    Credentials validation

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -30,3 +30,12 @@ class Auth:
         except NoResultFound:
             user = self._db.add_user(email, _hash_password(password))
         return user
+
+    def valid_login(self, email: str, password: str) -> bool:
+        """ Valid login """
+        try:
+            user = self._db.find_user_by(email=email)
+            return bcrypt.checkpw(password.encode('utf-8'),
+                                  user.hashed_password)
+        except Exception:
+            return False
